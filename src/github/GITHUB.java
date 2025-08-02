3bd17bc9ea4c6af45200053b17c5d897233e3d54
valex <valex@192.168.34.58>
2025-08-02 17:04:17 -0600

    "cambios 02-08-2025!"

---
+++ src/github/GITHUB.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author valex
+ */
+public class GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre, edad; 
+        Scanner lector=new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+        nombre=lector.next();
+        System.out.println("Ingrese su edad");
+        edad=lector.next();
+    }
+    
+}
